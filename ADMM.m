a58cc8b95eb8f8cc4b7e88b4cc282045c1bee9dc
Aaron Myers <aaron.myers@utexas.edu>
2014-12-26 10:59:56 -0600

    Updating files

--- ADMM.m
+++ ADMM.m
@@ -1,5 +1,4 @@
-
-function [z, history] = lasso(A, b, lambda, rho, alpha)
+function [z] = ADMM(A, b, lambda, rho, alpha)
 % lasso  Solve lasso problem via ADMM
 %
 % [z, history] = lasso(A, b, lambda, rho, alpha);
@@ -22,24 +21,20 @@ function [z, history] = lasso(A, b, lambda, rho, alpha)
 %
 % More information can be found in the paper linked at:
 % http://www.stanford.edu/~boyd/papers/distr_opt_stat_learning_admm.html
-%
-
-t_start = tic;
 
 [m, n] = size(A);
 
 % save a matrix-vector multiply
 Atb = A'*b;
-ADMM solver
 
-x = zeros(n,1);
+%x = zeros(n,1);
 z = zeros(n,1);
 u = zeros(n,1);
 
-MAX_ITER = 15;
+MAX_ITER = 3;
 
 % cache the factorization
-[L U] = factor(A, rho);
+[L, U] = factorit(A, rho);
 
 
 for k = 1:MAX_ITER
@@ -47,54 +42,55 @@ for k = 1:MAX_ITER
     % x-update
     q = Atb + rho*(z - u);    % temporary value
     if( m >= n )    % if skinny
-       x = U \ (L \ q);
+       x = U \ (L \ q)
     else            % if fat
-       x = q/rho - (A'*(U \ ( L \ (A*q) )))/rho^2;
+       x = q./rho - (A'*(U \ ( L \ (A*q) )))./rho^2;
     end
 
     % z-update with relaxation
     zold = z;
 
     x_hat = alpha*x + (1 - alpha)*zold;
-    z = shrinkage(x_hat + u, lambda/rho);
+    lambda/rho
+    z = shrinkage(x_hat + u, lambda/rho)
 
     % u-update
-    u = u + (x_hat - z);
+    u = u + (x_hat - z)
 
-    % diagnostics, reporting, termination checks
-    history.objval(k)  = objective(A, b, lambda, x, z);
-
-    history.r_norm(k)  = norm(x - z);
-    history.s_norm(k)  = norm(-rho*(z - zold));
-
-    history.eps_pri(k) = sqrt(n)*ABSTOL + RELTOL*max(norm(x), norm(-z));
-    history.eps_dual(k)= sqrt(n)*ABSTOL + RELTOL*norm(rho*u);
-
-
-
-            history.r_norm(k), history.eps_pri(k), ...
-            history.s_norm(k), history.eps_dual(k), history.objval(k));
-
-
-    if (history.r_norm(k) < history.eps_pri(k) && ...
-       history.s_norm(k) < history.eps_dual(k))
-         break;
-    end
-
-end
-
+%     % diagnostics, reporting, termination checks
+%     history.objval(k)  = objective(A, b, lambda, x, z);
+% 
+%     history.r_norm(k)  = norm(x - z);
+%     history.s_norm(k)  = norm(-rho*(z - zold));
+% 
+%     history.eps_pri(k) = sqrt(n)*ABSTOL + RELTOL*max(norm(x), norm(-z));
+%     history.eps_dual(k)= sqrt(n)*ABSTOL + RELTOL*norm(rho*u);
+% 
+% 
+% 
+%             history.r_norm(k), history.eps_pri(k), ...
+%             history.s_norm(k), history.eps_dual(k), history.objval(k));
+% 
+% 
+%     if (history.r_norm(k) < history.eps_pri(k) && ...
+%        history.s_norm(k) < history.eps_dual(k))
+%          break;
+%     end
 
 end
 
-function p = objective(A, b, lambda, x, z)
-    p = ( 1/2*sum((A*x - b).^2) + lambda*norm(z,1) );
+
 end
+
+% function p = objective(A, b, lambda, x, z)
+%     p = ( 1/2*sum((A*x - b).^2) + lambda*norm(z,1) );
+% end
 
 function z = shrinkage(x, kappa)
     z = max( 0, x - kappa ) - max( 0, -x - kappa );
 end
 
-function [L U] = factor(A, rho)
+function [L, U] = factorit(A, rho)
     [m, n] = size(A);
     if ( m >= n )    % if skinny
        L = chol( A'*A + rho*speye(n), 'lower' );
